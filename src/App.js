22380d112c6cd056c89ae0fc7568f1fe296424e2
Fidel Collin <fidelcollin@icloud.com>
2021-01-26 18:36:31 +0000

    broken ourhoempage into smaller components and also finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./components/pages/homepage/Homepage";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
